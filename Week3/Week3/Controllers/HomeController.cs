8b7dcbe383be17d6bab939b61c83adce27ade288
navchhibber <navi.7785@gmail.com>
2020-02-07 12:19:30 -0500

    Navjoot's commit

--- Week3/Week3/Controllers/HomeController.cs
+++ Week3/Week3/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace Week3.Controllers
         {
             return View();
         }
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
