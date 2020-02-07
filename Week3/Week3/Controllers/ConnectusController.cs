e225833cd85526460233142f623f1370d4e9e000
navchhibber <navi.7785@gmail.com>
2020-02-07 11:40:25 -0500

    Suhail's commit

---
+++ Week3/Week3/Controllers/ConnectusController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week3.Models;
+
+namespace Week3.Controllers
+{
+    public class ConnectusController : Controller
+    {
+        [HttpGet]
+        public IActionResult Connectus()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Connectus(Connectus contact)
+        {
+            return View(contact);
+        }
+    }
+}
---
+++ Week3/Week3/Models/Connectus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Week3.Models;
+
+namespace Week3.Models
+{
+    public class Connectus
+    { 
+
+        public string Name { get; set; }
+        public string Email{ get; set; }
+        public string Number { get; set; }
+        public string Message { get; set; }
+        public DateTime Date{ get; set; }
+
+    }
+}
